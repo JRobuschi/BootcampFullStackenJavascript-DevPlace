374a92b5b7e17f324c8b3fb84041a5a9c8760e53
Robuschi Juan Cruz <juanrobuschi5@gmail.com>
2022-11-30 12:24:40 -0300

    challenge

--- actividadesIndividuales/challengeNode/routes/recurso.route.js
+++ actividadesIndividuales/challengeNode/routes/recurso.route.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const recurso = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const {
   getRecurso,
   createRecurso,
@@ -10,9 +12,44 @@ const {
 
 /**Get all users */
 recurso.get("/", getRecurso);
-recurso.post("/", createRecurso);
 recurso.get("/:id", findByRecurso);
-recurso.put("/:id", UpdateByRecurso);
-recurso.delete("/:id", deleteByRecurso);
+recurso.post("/login/create", createRecurso);
+recurso.put("/login/update/:id", UpdateByRecurso);
+recurso.delete("admin//update/:id", deleteByRecurso);
+
+recurso.post("/login", function (req, res) {
+  let body = req.body;
+  User.findOne({ email: body.email }, (error, usuarioDB) => {
+    if (error) {
+      return res.status(500).json({
+        ok: false,
+        err: erro,
+      });
+    }
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "Usuario o contraseña incorrecta",
+        },
+      });
+    }
+
+    let token = jwt.sign(
+      {
+        username: usuarioDB,
+      },
+      process.env.SEED_AUTENTICACION,
+      {
+        expiresIn: process.env.CADUCIDAD_TOKEN,
+      }
+    );
+    res.json({
+      ok: true,
+      username: usuarioDB,
+      token,
+    });
+  });
+});
 
 module.exports = recurso;
